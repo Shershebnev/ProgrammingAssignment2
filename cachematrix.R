8193220221da0ea9a3dae48fc7abcca5f5cbe689
Shershebnev <prowtiger@gmail.com>
2014-08-22 17:06:04 +0400

    Second Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following functions cache the matrix invert 
 
-## Write a short comment describing this function
+## Produce a list of 4 methods: set - set the matrix, get - return the matrix,
+## setinvert - set invert of the matrix, getinvert - return the invert
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## (matrix) -> list of methods
+    ##
+    ## > b <- makeCacheMatrix(matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2))
+    ## > b$get()
+    ##      [,1] [,2]
+    ## [1,]    1    3
+    ## [2,]    2    4
+    
+    invert <- NULL
+    set <- function(y){
+        x <<- y
+        invert <<- NULL
+    }
+    get <- function() x
+    setinvert <- function(solve) invert <<- solve
+    getinvert <- function() invert
+    list(set = set, get = get,
+          setinvert = setinvert,
+          getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Calculate invert of the matrix, if it's the first time run
+## Otherwise return invert stored in cache 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## (list of methods) -> matrix
+    ##
+    ## > b <- makeCacheMatrix(matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2))
+    ## c <- cacheSolve(b)
+    ## > c
+    ##      [,1] [,2]
+    ## [1,]   -2  1.5
+    ## [2,]    1 -0.5
+    ## 
+    ## > b$get() %*% z
+    ##      [,1] [,2]
+    ## [1,]    1    0
+    ## [2,]    0    1
+    ##
+    ## Hooray, everything works. Now you can go on a drinking spree ^_^
+    
+    invert <- x$getinvert()
+    if(!is.null(invert)){
+        message("Getting cached data")
+        return(invert)
+    }
+    data <- x$get()
+    invert <- solve(data, ...)
+    x$setinvert(invert)
+    invert
 }
